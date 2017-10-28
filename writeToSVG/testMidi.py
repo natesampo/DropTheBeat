9f56c23ef1a02debd982648db6de50839ed6535f
CleanestMink126 <gns126@gmail.com>
2017-10-28 14:21:42 -0400

    successfully integrated midi and svg cut

--- writeToSVG/testMidi.py
+++ writeToSVG/testMidi.py
@@ -1,29 +1,27 @@
 import mido
 import numpy as np
-mid = mido.MidiFile('bach_bourree.mid')
-#messages go from c to b
-baseNote = 70
-noterange = 5
-for i, track in enumerate(mid.tracks):
-    print('Track {}: {}'.format(i, track.name))
-    # for msg in track:
-    #     print(msg)
-emptyNotes = np.zeros(noterange)
-currentNotes = emptyNotes.copy()
-timeTracker = []
-notesList = []
-elapsedTime = 0
-for msg in mid.tracks[1]:
-    if msg.time != 0:
-        elapsedTime += msg.time
-        if max(currentNotes):
-            timeTracker.append(elapsedTime)
-            notesList.append(currentNotes)
-            currentNotes = emptyNotes.copy()
-            elapsedTime = 0
 
-    if msg.type == 'note_on' and baseNote <= msg.note < baseNote + noterange:
-        currentNotes[msg.note - baseNote] = 1
-    print(msg)
+def printTracks(fileName):
+    mid = mido.MidiFile(fileName)
+    for i, track in enumerate(mid.tracks):
+        print('Track {}: {}'.format(i, track.name))
 
-print(notesList)
+
+def readMidi(fileName, trackNumber = 1, baseNote = 0, noteRange = 5):
+    mid = mido.MidiFile(fileName)
+    emptyNotes = np.zeros(noteRange)
+    currentNotes = emptyNotes.copy()
+    timeTracker = []
+    notesList = []
+    elapsedTime = 0
+    for msg in mid.tracks[1]:
+        if msg.time != 0:
+            elapsedTime += msg.time
+            if max(currentNotes):
+                timeTracker.append(elapsedTime)
+                notesList.append(currentNotes)
+                currentNotes = emptyNotes.copy()
+                elapsedTime = 0
+        if msg.type == 'note_on' and baseNote <= msg.note < baseNote + noteRange:
+            currentNotes[msg.note - baseNote] = 1
+    return timeTracker, np.array(notesList)
--- writeToSVG/testSVG.py
+++ writeToSVG/testSVG.py
@@ -1,48 +1,52 @@
 import svgwrite
 from svgwrite import mm, cm
 import numpy as np
+import testMidi
 
 
-sw = 5
+
+strokeColor = "red"
 svgname = 'test.svg'
-class svgCut:
-    def __init__(self,numHoles,holeSize,holeSpace,borderSpace):
-        self.numHoles = numHoles
-        self.holeSize = holeSize
-        self.holeSpace = holeSpace
-        self.borderSpace = borderSpace
-        self.pagex = self.borderSpace * 2 + (self.numHoles + 1) * self.holeSpace
+mm = 3.543
+cm = 35.43
+inch = 90
+sw = 0.001
+sysMult = inch
 
-    def drawBorders():
+print(sysMult)
+class svgCut:
+    def __init__(self,numHoles,holeRadius,holeSpace,borderSpace):
+        self.numHoles = numHoles
+        self.holeRadius = holeRadius * sysMult
+        self.holeSpacex = holeSpace[0] * sysMult
+        self.holeSpacey = holeSpace[1] * sysMult
+        self.borderSpace = borderSpace * sysMult
+        self.pagex = self.borderSpace * 2 + (self.numHoles - 1) * self.holeSpacex + self.holeRadius * 2
+
+    def drawRectangle():
         #TODO
         pass
 
     def drawHole(self, dwg, point):
-        dwg.add(dwg.circle(center=(self.borderSpace + self.holeSpace * point[0], self.holeSpace * point[1]),
-                            r=self.holeSize,fill_opacity = 0.0,stroke="black",stroke_width=sw))
+        dwg.add(dwg.circle(center=(self.borderSpace + self.holeSpacex * point[0] + self.holeRadius, self.holeSpacey * point[1] + self.holeRadius),
+                            r=self.holeRadius,fill_opacity = 0.0,stroke=strokeColor,stroke_width=sw))
 
     def drawHoles(self,holeArray):
         if(len(holeArray[0]) != self.numHoles):
             return 0
-        self.pagey = (self.numHoles + 1) * self.holeSpace
+        self.pagey = (len(holeArray) + 1) * self.holeSpacey
         xString = str(int(self.pagex)) + 'px'
         yString = str(int(self.pagey)) + 'px'
         print(xString)
         dwg = svgwrite.Drawing(svgname, size = (xString, yString))
         for i,line in enumerate(holeArray):
-            [self.drawHole(dwg,(j+1,i+1)) for j,v in enumerate(line) if v]
+            [self.drawHole(dwg,(j,i)) for j,v in enumerate(line) if v]
         dwg.save()
 
 
 
 
 if __name__ == "__main__":
-    cutObj = svgCut(numHoles = 5,holeSize = 30,holeSpace = 60, borderSpace= 50)
-    k = np.array([
-                [0,0,0,0,0],
-                [0,1,1,0,0],
-                [1,1,0,0,1],
-                [0,0,1,0,0],
-                [0,0,1,0,0]
-                ],dtype = "uint8")
-    cutObj.drawHoles(k)
+    cutObj = svgCut(numHoles = 9,holeRadius = .125,holeSpace = [.25,.25], borderSpace= 1)
+    times, cutArr = testMidi.readMidi('bach_bourree.mid', trackNumber = 1, baseNote = 71, noteRange = 9)
+    cutObj.drawHoles(cutArr)
