3163e173a2274b237e10147e7552adaa09556d07
CleanestMink126 <gns126@gmail.com>
2017-10-28 15:16:18 -0400

    documented progress

--- writeToSVG/testMidi.py
+++ writeToSVG/testMidi.py
@@ -2,26 +2,34 @@ import mido
 import numpy as np
 
 def printTracks(fileName):
+    #print all the instuments in the midi file
     mid = mido.MidiFile(fileName)
     for i, track in enumerate(mid.tracks):
         print('Track {}: {}'.format(i, track.name))
 
 
-def readMidi(fileName, trackNumber = 1, baseNote = 0, noteRange = 5):
-    mid = mido.MidiFile(fileName)
-    emptyNotes = np.zeros(noteRange)
+def readMidi(fileName, trackNumbers = [1], baseNote = 0, noteRange = 5, noteLim = 30):
+    '''This will read a midi file and return an array of notes in the given range (x size )
+    and up to the given length  (y size). The trackNumbers dictates which tracks to accept notes from
+    it will also return an array of times dictating how long to wait before droping the current set.'''
+    mid = mido.MidiFile(fileName) #get midi object
+    emptyNotes = np.zeros(noteRange) #empty range
     currentNotes = emptyNotes.copy()
     timeTracker = []
     notesList = []
     elapsedTime = 0
-    for msg in mid.tracks[1]:
-        if msg.time != 0:
+    for msg in mid: # run through all messages
+        if msg.time != 0: # if there is a time associated with the message add it to time tracker
             elapsedTime += msg.time
-            if max(currentNotes):
+            if max(currentNotes): #if there is a note played, index current states and move on to next row
                 timeTracker.append(elapsedTime)
                 notesList.append(currentNotes)
                 currentNotes = emptyNotes.copy()
                 elapsedTime = 0
-        if msg.type == 'note_on' and baseNote <= msg.note < baseNote + noteRange:
+
+        if (not msg.is_meta) and  msg.channel in trackNumbers and msg.type == 'note_on' and baseNote <= msg.note < baseNote + noteRange:
+            #if its a valid message and track in the key range, write 1 in the corresponding index.
             currentNotes[msg.note - baseNote] = 1
+        if len(timeTracker) > noteLim:#if we get too long, break
+            break
     return timeTracker, np.array(notesList)
--- writeToSVG/testSVG.py
+++ writeToSVG/testSVG.py
@@ -5,41 +5,45 @@ import testMidi
 
 
 
-strokeColor = "red"
-svgname = 'test.svg'
-mm = 3.543
+strokeColor = "red" #
+svgname = 'test.svg'#file to load
+mm = 3.543 #simple conversions between px and other formats
 cm = 35.43
 inch = 90
-sw = 0.001
-sysMult = inch
+sw = 0.001 * inch #width of drawn lines
+sysMult = inch # chosen multiplier for all inputs
 
 print(sysMult)
 class svgCut:
+    '''This is the object that controls most of the operations of writing to the
+    svg'''
     def __init__(self,numHoles,holeRadius,holeSpace,borderSpace):
         self.numHoles = numHoles
         self.holeRadius = holeRadius * sysMult
-        self.holeSpacex = holeSpace[0] * sysMult
+        self.holeSpacex = holeSpace[0] * sysMult #x distance between holes.. should be at least radius * 2
         self.holeSpacey = holeSpace[1] * sysMult
-        self.borderSpace = borderSpace * sysMult
-        self.pagex = self.borderSpace * 2 + (self.numHoles - 1) * self.holeSpacex + self.holeRadius * 2
+        self.borderSpace = borderSpace * sysMult #extra space on the sides where no holes will be drawn
+        self.pagex = self.borderSpace * 2 + (self.numHoles - 1) * self.holeSpacex + self.holeRadius * 2 #from what we are already given we can calculate width of page
 
     def drawRectangle():
+        '''This will draw a rectangle around the border so it can be properly cut'''
         #TODO
         pass
 
     def drawHole(self, dwg, point):
+        '''quick code to add a hold to the previous specifications'''
         dwg.add(dwg.circle(center=(self.borderSpace + self.holeSpacex * point[0] + self.holeRadius, self.holeSpacey * point[1] + self.holeRadius),
                             r=self.holeRadius,fill_opacity = 0.0,stroke=strokeColor,stroke_width=sw))
 
     def drawHoles(self,holeArray):
-        if(len(holeArray[0]) != self.numHoles):
+        '''draw all holes given an array of positions'''
+        if(len(holeArray[0]) != self.numHoles):#check to make sure size of  array is correct
             return 0
-        self.pagey = (len(holeArray) + 1) * self.holeSpacey
-        xString = str(int(self.pagex)) + 'px'
+        self.pagey = (len(holeArray) + 1) * self.holeSpacey #calculate pag length
+        xString = str(int(self.pagex)) + 'px' #string stuff
         yString = str(int(self.pagey)) + 'px'
-        print(xString)
         dwg = svgwrite.Drawing(svgname, size = (xString, yString))
-        for i,line in enumerate(holeArray):
+        for i,line in enumerate(holeArray): #cut holes in array where element  != 0
             [self.drawHole(dwg,(j,i)) for j,v in enumerate(line) if v]
         dwg.save()
 
@@ -47,6 +51,7 @@ class svgCut:
 
 
 if __name__ == "__main__":
-    cutObj = svgCut(numHoles = 9,holeRadius = .125,holeSpace = [.25,.25], borderSpace= 1)
-    times, cutArr = testMidi.readMidi('bach_bourree.mid', trackNumber = 1, baseNote = 71, noteRange = 9)
+    cutObj = svgCut(numHoles = 36,holeRadius = .125,holeSpace = [.25,.25], borderSpace= 1)
+    testMidi.printTracks('chopstik.mid')
+    times, cutArr = testMidi.readMidi('chopstik.mid', trackNumbers = [1,2,3,4,5], baseNote = 36, noteRange = 36, noteLim = 100)
     cutObj.drawHoles(cutArr)
