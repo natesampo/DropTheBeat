57a553b63b196d3431a76bfa59485ec8ca804065
CleanestMink126 <gns126@gmail.com>
2017-12-14 11:22:35 -0500

    added documentation

--- stepperDRV/stepperDRV.ino
+++ stepperDRV/stepperDRV.ino
@@ -1,20 +1,35 @@
 int wait = 10; 
 #include <AccelStepper.h>
 AccelStepper sheets(1, 11,10); // pin 3 = step, pin 6 = direction
-AccelStepper wheel(1, 12,9); // pin 3 = step, pin 6 = direction
+//AccelStepper wheel(1, 12,9); // pin 3 = step, pin 6 = direction
 
 void setup()
 {
   sheets.setMaxSpeed(400);
   sheets.setSpeed(320);
-  wheel.setMaxSpeed(100);
-  wheel.setSpeed(25);
+//  analogWrite(9, 60);
+  digitalWrite(6,HIGH);
    
 }
 
 void loop()
 {
-  wheel.runSpeed();
+  if(digitalRead(5)){
+    sheets.setSpeed(320);
+    sheets.runSpeed();
+  }else if(digitalRead(7)){
+    long currentMillis = millis();
+//
 
+     if (currentMillis % 3000 < 0){
+        analogWrite(9, 80);
+     }else{
+        analogWrite(9, 40);
+     }
+     sheets.setSpeed(0);
+   }else{
+      analogWrite(9, 0);
+      sheets.setSpeed(0);
+   }
 //  
 }
--- writeToSVG/writeSVG.py
+++ writeToSVG/writeSVG.py
@@ -1,21 +1,15 @@
+#Author: Nick Steelman
+#Last Revised Date: 12/14/17
+#Contact: steelman@students.olin.edu
+
 import svgwrite
 from svgwrite import mm, cm
 import numpy as np
 import convertMidi
 
-strokeColor = "red" #
-svgname = 'MountainKing.svg'#file to load
-# mm = 3.543 #simple conversions between px and other formats
-# cm = 35.43
-# inch = 90
-sw = str(0.1)#width of drawn lines
-sysMult = 1 # chosen multiplier for all inputs
-# sysDict = {3.543:'mm',35.43:'cm',90:'inches'}
-print(sysMult)
-honeyComb = 0
-def convertIn(unit):
-    return str(unit)
 
+def convertIn(unit):
+    return str(unit)#this is unnecessary code that could be implemented to convert units
 class svgCut:
     '''This is the object that controls most of the operations of writing to the
     svg'''
@@ -26,10 +20,11 @@ class svgCut:
         self.holeRadius = holeRadius
         self.holeSpacex = holeSpace[0] #x distance between holes.. should be at least radius * 2
         self.holeSpacey = holeSpace[1]
-        self.borderSpacex = borderSpace[0]
-        self.borderSpacey =  borderSpace[1]#extra space on the sides where no holes will be drawn
-        self.borderSpaceEnd = borderSpace[2]
-        if honeyComb:
+        self.borderSpacex = borderSpace[0]#extra space at beginning of the song
+        self.borderSpacey =  borderSpace[1]#extra space at the end of the song
+        self.borderSpaceEnd = borderSpace[2]#extra space on the sides where no holes will be drawn
+        if honeyComb:#determine the sheet width based on number of holes, spacebetween holes,
+        #and whether we are using a honeycomb pattern in hole drawing
             self.pagex = self.borderSpacex * 2 + (self.numHoles-0.5) * self.holeSpacex + 2*holeRadius
         else:
             self.pagex = self.borderSpacex * 2 + (self.numHoles - 1) * self.holeSpacex + self.holeRadius * 2 #from what we are already given we can calculate width of page
@@ -38,11 +33,15 @@ class svgCut:
         '''This will draw a rectangle around the border so it can be properly cut'''
         if end is None:
             end = (self.pagex,self.pagey)
+
+        #define a list of lines that need to be draw
         lines = [[origin, (origin[0],end[1])], [origin, (end[0],origin[1])],[end, (origin[0],end[1])], [end,  (end[0],origin[1])]]
-        for line in lines:
+        for line in lines:#draw each line in the list
             dwg.add(dwg.line(start = (convertIn(line[0][0]),convertIn(line[0][1])),end = (convertIn(line[1][0]),convertIn(line[1][1])),stroke=strokeColor,stroke_width=sw))
         pass
     def setSpeeds(self,slow, fast):
+        '''If you want to encode time into the sheet, input the min and max
+        speed the sheet can go (not used in final project)'''
         #in inches per second
         if honeyComb:
             vSpace = self.holeSpacey / 2
@@ -54,17 +53,20 @@ class svgCut:
 
 
     def getProportion(self, time):
+        '''math to calculate how to encode time into sheet (also not used)'''
         return (time - self.minTime)  / (self.maxTime - self.minTime)
 
 
     def encodeTimes(self,timeArr):
+        '''deprecated code calculating the size of squares to make to encode time into
+        the sheet'''
         lengthArr = []
         if honeyComb:
             vSpace = self.holeSpacey / 2
         else:
             vSpace = self.holeSpacey
 
-        for i in timeArr:
+        for i in timeArr:#
             if i > self.maxTime:
                 print("TOO LONG")
                 i = self.maxTime
@@ -76,6 +78,7 @@ class svgCut:
         return lengthArr
 
     def drawTimes(self,lengthArr,dwg,width = 1,topLeft = (0,0)):
+        '''deprecated code that would draw the rectangles found by encode times'''
         if honeyComb:
             vSpace = self.holeSpacey / 2
         else:
@@ -111,66 +114,72 @@ class svgCut:
         if(len(holeArray[0]) != self.numHoles):#check to make sure size of  array is correct
             print('DIMENSION MISMATCH')
             return 0
-        if honeyComb:
+        if honeyComb:#calculate length of the sheet using number of final notes and sizing of sheet
             self.pagey = (len(holeArray)//2 + 1) * self.holeSpacey +  self.borderSpacey + self.borderSpaceEnd
         else:
             self.pagey = (len(holeArray) + 1) * self.holeSpacey  +  self.borderSpacey + self.borderSpaceEnd#calculate pag length
         xString = str(self.pagex) + 'in' #string stuff
         yString = str(self.pagey) + 'in'
-        print('Width in inches: ',str(self.pagex))
+        print('Width in inches: ',str(self.pagex))#check length
         print('Height in inches: ',str(self.pagey))
-        dwg = svgwrite.Drawing(svgname, size = (xString, yString))
-        dwg.defs
-        dwg.viewbox(0,0,width = self.pagex, height = self.pagey)
+        dwg = svgwrite.Drawing(svgname, size = (xString, yString))#make a new drawing
+        dwg.viewbox(0,0,width = self.pagex, height = self.pagey)#set a viewbox in the drawing,
+        #this allows our our number to automatically be converted to inches
         for i,line in enumerate(holeArray): #cut holes in array where element  != 0
             [self.drawHole(dwg,(j,i)) for j,v in enumerate(line) if v]
         self.drawRectangle(dwg)
-        if timeArr is not None:
+        if timeArr is not None:#incode times into the sheet if so desired
             lengthArr = cutObj.encodeTimes(timeArr)
             cutObj.drawTimes(lengthArr,dwg,width = self.borderSpacex,topLeft = (self.pagex - self.borderSpacex,self.borderSpacey + self.holeRadius))
         return dwg
 
     def addHoles(self, timeArr, cutArr, minTime = 0):
-
+        '''This will take an array representing all the notes and what times they
+        were played and turn it into a larger array with whitespace to include the
+        notes of longer duration'''
         unit = min(timeArr)
         # timeArr[0] = 0
         if unit >= minTime:
             minTime = unit
         if self.minTime and minTime < self.minTime:
-            raise NameError('NOT FAST ENOUGH')
+            raise NameError('NOT FAST ENOUGH')#check to make sure quickest not is not too quick
         plus = 1
         empty = [0] * len(cutArr[0])
-        for i,v in enumerate(timeArr):
+        for i,v in enumerate(timeArr):#go through the cut array and inser zeros where appropriate
             extra = round(v/minTime)-1
-            print(extra)
+            # print(extra)
             for j in range(extra):
                 print('Done')
-
                 cutArr = np.insert(cutArr,i+ plus, empty, axis = 0)
                 plus += 1
-        print(cutArr)
+        # print(cutArr)
         return cutArr
 
 def convertToBeats(s):
-    #TODO
+    '''function that would potentially convert times to beats but was not used'''
     return s
 
 
+strokeColor = "red" #
+svgname = 'MountainKing.svg'#file to load
+sw = str(0.1)#width of drawn lines
+sysMult = 1 # chosen multiplier for all inputs, not needed
+honeyComb = 0
+
 if __name__ == "__main__":
-    svgname = 'mega.svg'
+    svgname = 'test2.svg'
+    #define a dictionary mapping notes to where they should go on the songsheet
     dictN = {64:0, 65:2, 66:4,67:6,68:8,69:10,70:12,71:1,72:3,73:5,74:7,75:9,76:11}
-    cutObj = svgCut(numHoles = 13,holeRadius = .265/2,holeSpace = [2*(.265 + 0.0751011),.265], borderSpace= [0.29899,2.5,2.5])
-    convertMidi.printTracks('megalovania.mid')
-    times, cutArr = convertMidi.readMidi('megalovania.mid', trackNumbers = list(range(0,2)), baseNote = 64, noteRange = 13, noteLim = 105,offset = 0,dictN = dictN)
-    print(times)
-    # cutObj.setSpeeds(slow = 0.1,fast = 2)
-
-    # print(times)
-    # print(lengthArr)
+    #define a svg cutter object with appropriate dimensions, gotten from the CAD
+    cutObj = svgCut(numHoles = 5,holeRadius = .265/2,holeSpace = [.265,.265], borderSpace= [.5,.5,.5])
+    #print the tracks from a desired MIDI file to see which ones to select
+    convertMidi.printTracks('MountainKing.mid')
+    #read the midi file and convert it into a cut array representing the order in
+    #which all notes are played and a time array representing how long each not lasts.
+    times, cutArr = convertMidi.readMidi('MountainKing.mid', trackNumbers = list(range(0,2)), baseNote = 64, noteRange = 13, noteLim = 105,offset = 0,dictN = dictN)
+    #add white space for longer notes
     cutArr = cutObj.addHoles(times, cutArr)
-
+    #add holes to the sheet
     dwg = cutObj.drawHoles(cutArr)
-
+    #save the svg
     dwg.save()
-    # svgname = 'pushStick.svg'
-    # cutObj.drawHoles([[0,0,0,0,0]])
